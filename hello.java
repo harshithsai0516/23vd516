af6d884018d89cf6c4ba6d4c158fd3fc464db16a
harshithsai0516 <harshithsaichiliveri@gmail.com>
2025-11-04 10:46:49 +0530

    initial commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello
+{
+public static void main (String[]args)
+{
+System.out.println("helloworld");
+int i=5,j=10;
+int c=i+j;
+int d=j-i;
+System.out.println("i+j= "+c);
+System.out.println("j-i= "+d);
+}
+}
